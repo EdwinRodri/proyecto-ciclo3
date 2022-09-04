1a0560f949ec30c6f83ef71e462889e2a8da2eb7
Edwin Alexander Rodriguez <edwinrodriguez955@gmail.com>
2022-09-04 14:47:08 -0500

    Primer push, creacion de templates

---
+++ src/app.py
@@ -0,0 +1,22 @@
+#importacion de todo lo necesario
+from flask import Flask, render_template
+
+app= Flask(__name__)
+
+#creamos la ruta al archivo html para que se visualice en el navegador
+@app.route('/')
+def pagina_inicio():
+    return render_template('inicio.html')#render_template hay que importarlo
+
+@app.route('/login')
+def login():
+    return render_template('login.html')#render_template hay que importarlo
+
+@app.route('/formulario_registro')
+def formulario_registro():
+    return render_template('formulario_registro.html')#render_template hay que importarlo
+
+
+#ejecutamos el servidor para que se actualice automaticamente
+if __name__ == '__main__':
+    app.run(debug=True, port=4555)
